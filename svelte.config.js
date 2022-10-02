2eaf91c879a7740cf787797348fdd7a8f9f91017
Aram Panasenco <panasenco@users.noreply.github.com>
2022-10-02 16:16:49 -0600

    My first Svelte browser extension now works!

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,24 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from 'sveltekit-adapter-browser-extension';
 import preprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown
+			pages: 'build', // the output directory for the pages of your extension
+			assets: undefined, // the asset output directory is derived from pages if not specified explicitly
+			fallback: undefined, // set to true to output an SPA-like extension
+			manifestVersion: 2, // the version of the automatically generated manifest (Version 3 is required by Chrome).
+            manifest: {
+                // You can customize manifest here
+            }
+        }),
+		appDir: 'ext', // This is important - chrome extensions can't handle the default _app directory name.
+		prerender: {
+			default: true
+		}
 	}
 };
-
-export default config;
