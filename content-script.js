5c5bbef7daaa41a1c9b7363933a6d738effe3ba4
Aram Panasenco <panasenco@users.noreply.github.com>
2022-10-10 16:14:06 -0600

    Now prepending some content to the job description

--- content-script.js
+++ content-script.js
@@ -1,14 +1,23 @@
-var jdContainer = document.getElementById("vjs-container");
+const insertOptimizer = (descriptionContainer) => {
+  const optimizerDiv = document.createElement("div");
+  const optimizerContent = document.createTextNode("Optimizer");
+  optimizerDiv.appendChild(optimizerContent);
+  descriptionContainer.prepend(optimizerDiv);
+}
 
-var observer = new MutationObserver(
+var jobContainer = document.getElementById("vjs-container");
+
+var refreshObserver = new MutationObserver(
   (mutationList) => {
     var removedNodes = mutationList[0].removedNodes;
     if (removedNodes.length > 0) {
       if (removedNodes[0].id == "vjs-placeholder-container") {
-        console.log("Refresh!")
+        var jobFrame = document.getElementById("vjs-container-iframe");
+        var descriptionContainer = jobFrame.contentWindow.document.getElementById("jobDescriptionText");
+        insertOptimizer(descriptionContainer);
       }
     }
   }
 );
 
-observer.observe(jdContainer, {childList: true});
+refreshObserver.observe(jobContainer, {childList: true});
