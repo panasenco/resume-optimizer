68e203dc88944a6cb42488307015abb251989a12
Aram Panasenco <panasenco@users.noreply.github.com>
2024-03-21 05:56:32 +0000

    Got the refactored code to actually run!

--- resume_optimizer/optimize.py
+++ resume_optimizer/optimize.py
@@ -3,27 +3,25 @@ from multiprocessing import Pool
 from typing import Any
 
 
-from .chains import distribute_keywords, summarize_resume_sections, optimize_highlights
+from .assign import assign
+from .chains import extract_keywords, get_compatibility, summarize_resume_sections, insert_keywords
 
 
 def optimize_resume(*, resume: dict[str, Any], job_description: str, job_title: str) -> dict[str, Any]:
     default_highlights = [
         (
-            i,
             experience["position"],
             "\n".join(f"- {highlight}" for highlight in experience["highlights"]),
         )
-        for i, experience in enumerate(resume["work"])
+        for experience in resume["work"]
     ]
-
-    # Stage 1: Distribute keywords among resume sections and summarize resume sections in parallel
+    # Stage 1: Summarize resume sections and extract job description keywords in parallel
     with Pool(2) as pool:
-        position_keywords_result = pool.apply_async(
-            func=distribute_keywords,
+        keywords_result = pool.apply_async(
+            func=extract_keywords,
             kwds={
                 "job_description": job_description,
                 "job_title": job_title,
-                "position_highlights": default_highlights,
             },
         )
         position_summaries_result = pool.apply_async(
@@ -32,24 +30,47 @@ def optimize_resume(*, resume: dict[str, Any], job_description: str, job_title: 
                 "position_highlights": default_highlights,
             },
         )
-        position_keywords = position_keywords_result.get()
+        keywords = keywords_result.get()
         position_summaries = position_summaries_result.get()
-    print("position_keywords=")
-    print(json.dumps(position_keywords, indent=4))
+    print("keywords=")
+    print(json.dumps(keywords, indent=4))
     print("---")
     print("position_summaries=")
     print(json.dumps(position_summaries, indent=4))
     print("---")
 
-    # Stage 2: Generate optimized highlights for each resume section.
+    # Stage 2: Assign keywords to resume sections while maximizing overall compatibility.
+    compatibility = get_compatibility(job_description_keywords=keywords, position_highlights=default_highlights)
+    print("compatibility=")
+    print("\n".join(["".join(map(str, row)) for row in compatibility]))
+    print("---")
+    highlight_counts = [3, 3, 2]
+    assignment = assign(compatibility=compatibility, count_weights=highlight_counts)
+    position_keywords = [
+        [
+            keywords[keyword_index]
+            for keyword_index, resume_section_index in assignment
+            if resume_section_index == current_resume_section_index
+        ]
+        for current_resume_section_index in range(len(default_highlights))
+    ]
+    print("position_keywords=")
+    print(json.dumps(position_keywords, indent=4))
+    print("---")
+    # Stage 3: Insert the keywords into the corresponding optimal summarized resume sections
 
     n_experiences = len(default_highlights)
     print("optimized_highlights=")
+    # Have to do a pool rather than run batch() on the chain because the chain itself must be changed depending on
+    # how many sections we have to return.
     with Pool(n_experiences) as pool:
         optimized_highlights = pool.starmap(
-            func=optimize_highlights,
+            func=insert_keywords,
             iterable=zip(
-                position_summaries, position_keywords, [[3, 3, 2][i] if i < 3 else 1 for i in range(n_experiences)]
+                position_summaries,
+                position_keywords,
+                [[3, 3, 2][i] if i < 3 else 1 for i in range(n_experiences)],
+                [60] * n_experiences,
             ),
         )
     print(optimized_highlights)
